b87c4a9dbadbe95ec8dee6f538e954d176d3938d
Alex Braga <contato@alexbraga.com.br>
2024-06-21 07:25:48 -0300

    Update `Main`

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.controller.DonationController;
+import org.example.dao.DonationDAO;
+import org.example.dao.DonationDAOImpl;
+import org.example.service.DonationService;
+import org.example.ui.ConsoleInterface;
+import org.example.util.MariaDBEntityManagerFactoryProvider;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        DonationDAO dao = new DonationDAOImpl(new MariaDBEntityManagerFactoryProvider());
+        DonationService service = new DonationService(dao);
+        DonationController controller = new DonationController(service);
+        ConsoleInterface ui = new ConsoleInterface(scanner, controller);
+
+        ui.mainMenu();
+        scanner.close();
     }
 }
